61ef1c56482e817ca9d12f67aeb92a3a4583a1cb
samndambuki <sam.ndambuki08@gmail.com>
2025-01-27 08:51:11 +0300

    installed bootstrap and currently working on fetching movies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  return (
+    <div className="App">
+      <p>This is movies app</p>
+    </div>
+  );
+}
+
+export default App;
