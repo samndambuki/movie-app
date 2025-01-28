284198f7ca5c107317a46d60fcb5e4cd380423bf
samndambuki <sam.ndambuki08@gmail.com>
2025-01-28 19:06:41 +0300

    worked on search movie feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import MovieList from "./components/MovieList";
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=835d1be4`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
